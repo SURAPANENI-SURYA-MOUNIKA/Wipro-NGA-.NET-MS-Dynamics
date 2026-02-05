17d25a41d667b9c57ddd424a6f3b03f9a993871a
SURAPANENI-SURYA-MOUNIKA <mounikasurapaneni2003@gmail.com>
2026-02-05 20:03:25 +0530

    TypeScript

---
+++ Wipro-Day24-TypeScript/Addition/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const addition_1 = require("./addition");
+const result = (0, addition_1.add)(20, 30);
+console.log("Addition Result:", result);
+//# sourceMappingURL=main.js.map
---
+++ Wipro-Day24-TypeScript/Addition/main.ts
@@ -0,0 +1,4 @@
+import { add } from "./addition";
+
+const result = add(20, 30);
+console.log("Addition Result:", result);
